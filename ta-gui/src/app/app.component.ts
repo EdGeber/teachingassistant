a467c7b248ef6b799e80371fd6928d0de2aa26e8
EdGeber <egma@cin.ufpe.br>
2022-03-19 19:48:50 -0300

    Basic student registration GUI

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ta-gui';
+    student = new Student();
+}
+
+export class Student {
+    name  = "";
+    ssn   = "";
+    email = "";
 }
