4b329a78e23875ca7a977c05c0c49766e4491194
EdGeber <egma@cin.ufpe.br>
2022-03-24 09:03:34 -0300

    Remove unnecessary 'any' cast

--- ta-gui/src/app/global-code/utils.ts
+++ ta-gui/src/app/global-code/utils.ts
@@ -3,5 +3,5 @@ export type ErrorSource = string;
 export interface ErrorHandlers { [index: number]: () => void }
 
 export function DeepCloneProperties(obj: any): any {
-    return <any> JSON.parse(JSON.stringify(obj));
+    return JSON.parse(JSON.stringify(obj));
 }
