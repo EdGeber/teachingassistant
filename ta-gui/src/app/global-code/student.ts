cad71aaccffff075a2efac3ec0a1f8ac6bb555f5
EdGeber <egma@cin.ufpe.br>
2022-03-27 19:26:53 -0300

    Add fromJSON static method to Student

--- ta-gui/src/app/global-code/student.ts
+++ ta-gui/src/app/global-code/student.ts
@@ -11,6 +11,14 @@ export class Student{
         "Tests":        "",
     };
 
+    // wtf man constructor overloading is not allowed...
+    static fromJSON(studentJSON: string): Student {
+        let from: any = JSON.parse(studentJSON);
+        let s = new Student();
+        for(let key in from) s[key] = from[key];
+        return s;
+    }
+
     // public methods
     clone(): Student {
         let cloned = new Student();
